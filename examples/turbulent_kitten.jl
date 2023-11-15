8a43908be0de1352348be4905b5c462b06a2c93a
Gregory Wagner <wagner.greg@gmail.com>
2023-11-15 09:22:56 -0700

    Less expensive example

--- examples/turbulent_kitten.jl
+++ examples/turbulent_kitten.jl
@@ -7,11 +7,14 @@ output_name = "kitten"
 
 output_filename = output_name * ".jld2"
 output_moviename = output_name * ".mp4"
+image_path = joinpath(@__DIR__, image_filename)
 
-simulation = turbulent_image_simulation(image_filename, output_filename,
+simulation = turbulent_image_simulation(image_path, output_filename,
                                         advection = WENO(order=9),
-                                        z_pixels = 512)
+                                        z_pixels = 128)
 run!(simulation)
+
+# Make a nice movie
 
 bt = FieldTimeSeries(output_filename, "b")
 
@@ -20,6 +23,7 @@ bn = @lift interior(bt[$n], :, 1, :)
 
 Nx, Ny, Nz = size(simulation.model.grid)
 aspect = Nx / Nz
+
 fig = Figure(resolution=(600aspect, 600))
 ax = GLMakie.Axis(fig[1, 1])
 heatmap!(ax, bn, colormap=:grays)
@@ -31,6 +35,4 @@ record(fig, output_moviename, 1:Nt, framerate=48) do nn
     @info string("Drawing frame $nn of $Nt...")
     n[] = nn
 end
-
-display(fig)
 
